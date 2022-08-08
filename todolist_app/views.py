5c0f837f6134b6acd00521371a90cb83ca325f09
abdullahasim19 <abdullahasimlhe@gmail.com>
2022-08-08 23:26:00 +0500

    Edit and delete operations done

--- todolist_app/views.py
+++ todolist_app/views.py
@@ -1,3 +1,4 @@
+from ast import Try
 from asyncio import all_tasks
 from re import M
 from django.shortcuts import render,redirect
@@ -13,11 +14,29 @@ def todolist(request):
         form=TaskForm(request.POST or None)
         if form.is_valid():
             form.save()
-        messages.success(request,('New Task Added'))
+            messages.success(request,('New Task Added'))
         return redirect('todolist')
     else:
         all_tasks=TaskList.objects.all
         return render(request,'todolist.html',{'all_tasks':all_tasks})
+
+def delete_task(request,task_id):
+    task=TaskList.objects.get(pk=task_id) # fetching the task
+    task.delete()
+    return redirect('todolist')
+
+def edit_task(request,task_id):
+    if request.method=='POST':
+        task=TaskList.objects.get(pk=task_id) # fetching the task
+        form=TaskForm(request.POST or None,instance=task)
+        if form.is_valid():
+            form.save()
+            messages.success(request,('Task Edited'))
+        print(form.errors)
+        return redirect('todolist')
+    else:
+        task_obj=TaskList.objects.get(pk=task_id)   
+        return render(request,'edit.html',{'task_obj':task_obj})
 
 def contact(request):
     context={
