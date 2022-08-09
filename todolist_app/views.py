fe01fac1deb49ac3fe2c64e0d4d5965bd64cf892
abdullahasim19 <abdullahasimlhe@gmail.com>
2022-08-09 21:44:00 +0500

    Pagination done

--- todolist_app/views.py
+++ todolist_app/views.py
@@ -6,6 +6,7 @@ from django.http import HttpResponse
 from todolist_app.models import TaskList
 from todolist_app.forms import TaskForm
 from django.contrib import messages
+from django.core.paginator import Paginator
 
 # Create your views here.
 
@@ -17,7 +18,10 @@ def todolist(request):
             messages.success(request,('New Task Added'))
         return redirect('todolist')
     else:
-        all_tasks=TaskList.objects.all
+        all_tasks=TaskList.objects.all()
+        paginator=Paginator(all_tasks,5)
+        page=request.GET.get('pg')
+        all_tasks=paginator.get_page(page)
         return render(request,'todolist.html',{'all_tasks':all_tasks})
 
 def delete_task(request,task_id):
@@ -62,3 +66,9 @@ def about(request):
         'about_text':"'Welcome About"
     }
     return render(request,'about.html',context)
+
+def index(request):
+    context={
+        'index_text':"'Welcome Index"
+    }
+    return render(request,'index.html',context)
